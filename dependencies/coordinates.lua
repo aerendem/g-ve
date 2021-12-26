afccc2ff52383d2b1dacd7afc67e785e63973ab8
Ali Eren <alieren.demireller@gmail.com>
2021-12-26 14:58:06 +0300

    add coordinates

---
+++ dependencies/coordinates.lua
@@ -0,0 +1,28 @@
+local coordinates = {}
+coordinates.__index = coordinates
+
+require("love")
+
+function coordinates.new(x, y)
+   local self = setmetatable({}, coordinates)
+
+   self.__index = self
+   self.x = x or nil
+   self.y = y or nil
+
+   return self
+end
+
+function coordinates:GetX()
+    return self.x
+end
+
+function coordinates:GetY()
+    return self.y
+end
+
+function coordinates:UnionWith()
+
+end
+
+return coordinates
